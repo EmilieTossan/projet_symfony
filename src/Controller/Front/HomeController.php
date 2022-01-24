31e68fd02985c7b0530f412ba2fced08b2ee8072
Émilie Tossan <emilietossan@MacBook-Pro-de-Emilie.home>
2022-01-24 17:09:39 +0100

    home

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class  HomeController extends AbstractController
+{
+    /**
+     * @Route("home", name="front_home")
+     */
+    public function home(CategoryRepository $categoryRepository)
+    {
+        $id = rand(1,50);
+        $category = $categoryRepository->find($id);
+
+        if($category){
+            return $this->render('front/home.html.twig', ['category' => $category]);
+        } else {
+            return $this->redirectToRoute('front_home');
+        }
+    }
+}
